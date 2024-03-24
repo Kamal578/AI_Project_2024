2363efa8457d4127566acd2d52f7aa2a5e3fff1b
Murad <muradmustafayev03@gmail.com>
2024-03-24 11:39:19 +0400

    docstrings added

--- DecisionTree.py
+++ DecisionTree.py
@@ -2,7 +2,13 @@ import numpy as np
 
 
 class DecisionTree:
+    """ Decision Tree Classifier """
+
     def __init__(self, max_depth=None, min_samples_split=None):
+        """
+        :param max_depth: maximum depth of the tree
+        :param min_samples_split: minimum number of samples required to split an internal node
+        """
         self.tree = None
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
@@ -11,15 +17,35 @@ class DecisionTree:
         self.n_features = None
 
     def fit(self, X, y):
+        """
+        Fit the model to the training data
+
+        :param X: training data
+        :param y: target values
+        """
         self.n_classes = len(np.unique(y))
         self.n_features = X.shape[1]
 
         self.tree = self._grow_tree(X, y)
 
     def predict(self, X):
+        """
+        Predict the target values
+
+        :param X: input data
+        :return: predicted target values
+        """
         return np.array([self._predict(inputs) for inputs in X])
 
     def _grow_tree(self, X, y, depth=0):
+        """
+        Recursively grow the tree
+
+        :param X: input data
+        :param y: target values
+        :param depth: current depth of the tree
+        :return: node of the tree if it is a leaf, otherwise a tuple of the split feature, threshold, left and right trees
+        """
         n_samples = X.shape[0]
         n_labels = [np.sum(y == i) for i in range(self.n_classes)]
         most_common_label = np.argmax(n_labels)
@@ -42,6 +68,13 @@ class DecisionTree:
         return feature, threshold, left_tree, right_tree
 
     def _best_split(self, X, y):
+        """
+        Find the best split for the data
+
+        :param X: input data
+        :param y: target values
+        :return: the best feature and threshold to split the data
+        """
         best_gini = 1
         best_feature = None
         best_threshold = None
@@ -61,6 +94,13 @@ class DecisionTree:
         return best_feature, best_threshold
 
     def _gini(self, y_left, y_right):
+        """
+        Calculate the Gini impurity
+
+        :param y_left: target values of the left node
+        :param y_right: target values of the right node
+        :return: Gini impurity
+        """
         n_left = len(y_left)
         n_right = len(y_right)
         n_total = n_left + n_right
@@ -71,6 +111,12 @@ class DecisionTree:
         return (n_left / n_total) * gini_left + (n_right / n_total) * gini_right
 
     def _predict(self, inputs):
+        """
+        Predict the target value of the input data
+
+        :param inputs: input data
+        :return: predicted target value
+        """
         node = self.tree
         while isinstance(node, tuple):
             feature, threshold, left_tree, right_tree = node
@@ -80,6 +126,3 @@ class DecisionTree:
                 node = right_tree
 
         return node
-
-    def __repr__(self):
-        return str(self.tree)
