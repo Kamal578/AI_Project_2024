3a93ed31ddaef5c805aa1350d9bac6b56c6578ad
Murad <muradmustafayev03@gmail.com>
2024-03-24 12:40:06 +0400

    tests optimized

--- test/RandomForestTest.py
+++ test/RandomForestTest.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from src.RandomForest import RandomForest
-from utils import mae, mse, max_dif
+from utils import categorical_accuracy, mae_accuracy, mse_accuracy, max_dif
 
 ds_path_1 = '../data/winequality-red_NO_ALCOHOL.csv'
 ds_path_2 = '../data/winequality-white_NO_ALCOHOL.csv'
@@ -33,13 +33,9 @@ def test_random_forest(dataset: pd.DataFrame):
 
     # Calculate the accuracy
     m = max_dif(y)
-    categorical_accuracy = np.mean(y_pred == y_test)
-    mae_accuracy = 1 - mae(y_test, y_pred) / m
-    mse_accuracy = 1 - mse(y_test, y_pred) / m ** 2
-
-    print(f'Categorical accuracy: {categorical_accuracy}')
-    print(f'MAE accuracy: {mae_accuracy}')
-    print(f'MSE accuracy: {mse_accuracy}')
+    print(f'Categorical accuracy: {categorical_accuracy(y_test, y_pred)}')
+    print(f'MAE accuracy: {mae_accuracy(m, y_test, y_pred)}')
+    print(f'MSE accuracy: {mse_accuracy(m, y_test, y_pred)}')
 
 
 if __name__ == '__main__':
--- test/utils.py
+++ test/utils.py
@@ -1,12 +1,45 @@
 import numpy as np
 
+
 def mae(y_true, y_pred):
+    """ Mean Absolute Error """
     return np.mean(np.abs(y_true - y_pred))
 
 
 def mse(y_true, y_pred):
+    """ Mean Squared Error """
     return np.mean((y_true - y_pred) ** 2)
 
 
 def max_dif(y):
+    """ Maximum difference between the target values """
     return np.max(y) - np.min(y)
+
+
+def categorical_accuracy(y_true, y_pred):
+    """ Categorical accuracy """
+    return np.mean(y_true == y_pred)
+
+
+def mae_accuracy(m, y_true, y_pred):
+    """
+    Mean Absolute Error based Accuracy
+
+    :param m: maximum difference between the target values
+    :param y_true: true target values
+    :param y_pred: predicted target values
+    :return: accuracy
+    """
+    return 1 - mae(y_true, y_pred) / m
+
+
+def mse_accuracy(m, y_true, y_pred):
+    """
+    Mean Squared Error based Accuracy
+
+    :param m: maximum difference between the target values
+    :param y_true: true target values
+    :param y_pred: predicted target values
+    :return: accuracy
+    """
+    return 1 - mse(y_true, y_pred) / m ** 2
